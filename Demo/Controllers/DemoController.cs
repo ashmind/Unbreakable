b45314093b82c1bfe22e6d0ebc82d871dd75f271
Andrey Shchekin <ashmind@gmail.com>
2017-06-05 00:56:30 +1200

    Corrected build process for Azure.

--- Demo/Controllers/DemoController.cs
+++ Demo/Controllers/DemoController.cs
@@ -4,7 +4,6 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Text;
 using System.Web.Mvc;
 using AppDomainToolkit;
 using AshMind.Extensions;
